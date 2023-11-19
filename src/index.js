d2f585340d184edf4b62492a68762841d285066f
Abdelilah <abdelilahworkset@gmail.com>
2023-11-19 14:32:30 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
-import * as crypto from "crypto";
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const crypto = __importStar(require("crypto"));
 class Vault {
-    constructor(secret_key, secret_iv = null) {
+    constructor(secret_key, secret_iv = "") {
         this.secret_key = Buffer.from(this.createHash("sha256", secret_key), 'hex');
         this.secret_iv = !secret_iv ? this.secret_iv = crypto.randomBytes(16) : Buffer.from(this.createHash("md5", secret_iv), "hex");
         this.data = new Map();
@@ -41,5 +66,4 @@ class Vault {
         return decrypted;
     }
 }
-export default Vault;
-//# sourceMappingURL=index.js.map
+exports.default = Vault;
